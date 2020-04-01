4c5b0240185d3b817a63c7a2e9e471fbd0807df6
JiaqiJin96 <fdsam@hotmail.com>
2020-04-01 20:08:21 +0200

    window creation

---
+++ DirectX/DirectX/Framework/Common/GraphicsManager.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../Interface/IRuntimeModule.h"
+
+namespace Kawaii
+{
+	class GraphicsManager : implements IRuntimeModule
+	{
+	public:
+		virtual ~GraphicsManager();
+	};
+}
